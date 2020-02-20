a585d832ed5d5b398a651b48b559ef283b9d843a
crypticle <35649100+crypticle@users.noreply.github.com>
2020-02-20 01:43:26 -0500

    Added project folder.
    Script will launch browser and go to target website and click through buttons

---
+++ EmpireAutoWithdraw/EmpireAutoWithdraw/AutoWithdrawTool.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace EmpireAutoWithdraw
+{
+    class AutoWithdrawTool
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver ChromeDriver = new ChromeDriver();
+
+            ChromeDriver.Navigate().GoToUrl("https://csgoempire.com/");
+
+            IWebElement withdraw = ChromeDriver.FindElement(By.XPath("//a[contains(@href, '/withdraw')]"));
+
+            if(withdraw.Displayed)
+            {
+                Console.WriteLine("Awesome the element was found!");
+            }
+            else
+            {
+                Console.WriteLine("Oh no! Element not found");
+            }
+
+            withdraw.Click();
+
+            Thread.Sleep(1000);
+
+            // IWebElement dotaButton = ChromeDriver.FindElement(By.XPath("//*[@class='pl-2']")); // Works for first button
+
+            IWebElement dotaButton = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Dota 2 ')]"));
+
+
+            if (dotaButton.Displayed)
+            {
+                Console.WriteLine("Awesome the element was found!");
+            }
+            else
+            {
+                Console.WriteLine("Oh no! Element not found");
+            }
+
+            dotaButton.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement itemButton = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Genuine Golden Gravelmaw ')]"));
+            itemButton.Click();
+
+            IWebElement itemButton2 = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Genuine Golden Gravelmaw ')] [2]"));
+            itemButton2.Click();
+            //Thread.Sleep(15000);
+            //ChromeDriver.Quit();
+        }
+    }
+}
+
+/*
+do
+{
+IList<IWebElement> Test= SeleniumDriver.WebDriver.FindElements(By.Xpath(""));
+
+//HERE YOU CHECK IF BUTTON EXIST
+if(isElementPresented(By.Id("")))
+{
+driver.FindElement(By.Id("")).Click;
+}
+else
+{
+//if button not exists
+buttonExist = false;
+}
+}while(buttonExist);
+*/
