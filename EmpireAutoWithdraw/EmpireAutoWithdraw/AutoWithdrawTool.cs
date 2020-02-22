46b24b58561a1a776566d518f88257405808f5fb
crypticle <35649100+crypticle@users.noreply.github.com>
2020-02-22 00:22:42 -0500

    Updated source code
    - Waits for user to login
    - Can now go to page two on withdraw
    - Feature added to search for item

--- EmpireAutoWithdraw/EmpireAutoWithdraw/AutoWithdrawTool.cs
+++ EmpireAutoWithdraw/EmpireAutoWithdraw/AutoWithdrawTool.cs
@@ -1,76 +1,117 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
+
 
 namespace EmpireAutoWithdraw
 {
     class AutoWithdrawTool
     {
+
+        static public void FindMyItem(IWebDriver driver, String itemName)
+        {
+            try
+            {
+                IWebElement findItem = driver.FindElement(By.XPath($"//img[@alt='{itemName}']"));
+                if (findItem.Displayed)
+                {
+                    Console.WriteLine("Item: Inscribed Wings of the Fireflight Scion Found!");
+                    for (int i = 0; i < 2; i++)
+                        Console.Beep();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Can't find item... Will try again in 2 minutes!");
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            IWebDriver ChromeDriver = new ChromeDriver();
 
+            String tradeURL = "https://steamcommunity.com/tradeoffer/new/?partner=61634773&token=msvftOMK";
+            String passCode = "0727";
+
+
+            // Creates WebDriver and goes to target website
+            IWebDriver ChromeDriver = new ChromeDriver();
             ChromeDriver.Navigate().GoToUrl("https://csgoempire.com/");
 
+            Console.WriteLine("Welcome to CSGOEmpire Auto-Withdraw tool!");
+            Console.WriteLine("Please login with Steam credentials then press Enter to continue.");
+            Console.ReadLine();
+
+
+            //Finds withdraw button on website
             IWebElement withdraw = ChromeDriver.FindElement(By.XPath("//a[contains(@href, '/withdraw')]"));
 
-            if(withdraw.Displayed)
+
+            //Checks if button is shown on site.
+            if (withdraw.Displayed)
             {
-                Console.WriteLine("Awesome the element was found!");
+                Console.WriteLine("Withdraw button found!");
+                withdraw.Click();
             }
             else
             {
                 Console.WriteLine("Oh no! Element not found");
             }
 
-            withdraw.Click();
-
             Thread.Sleep(1000);
 
-            // IWebElement dotaButton = ChromeDriver.FindElement(By.XPath("//*[@class='pl-2']")); // Works for first button
-
+            //Finds DotA 2 on Withdraw
             IWebElement dotaButton = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Dota 2 ')]"));
 
 
             if (dotaButton.Displayed)
             {
-                Console.WriteLine("Awesome the element was found!");
+                Console.WriteLine("DotA 2 Button found!");
+                dotaButton.Click();
             }
             else
             {
                 Console.WriteLine("Oh no! Element not found");
             }
 
-            dotaButton.Click();
+            Thread.Sleep(2000);
 
-            Thread.Sleep(1000);
+            bool loop = true;
 
+            String itemName = "Inscribed Wings of the Fireflight Scion";
+
+            while (loop)
+            {
+
+                // Code for waiting until item exists: https://stackoverflow.com/questions/19573943/webdriverwait-how-to-wait-until-an-item-exists-or-doesnt-exist
+
+                FindMyItem(ChromeDriver, itemName);
+
+                ChromeDriver.Navigate().Refresh();
+
+                ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            }
+
+            
+
+
+            // Clicks next page
+            IWebElement pageTwo = ChromeDriver.FindElement(By.XPath("//a[text()='2']"));
+            pageTwo.Click();
+
+            /* Finds item and clicks on it.
             IWebElement itemButton = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Genuine Golden Gravelmaw ')]"));
             itemButton.Click();
+            */
 
-            IWebElement itemButton2 = ChromeDriver.FindElement(By.XPath("//*[contains(text(), ' Genuine Golden Gravelmaw ')] [2]"));
-            itemButton2.Click();
+
+
             //Thread.Sleep(15000);
             //ChromeDriver.Quit();
         }
     }
+
+
 }
 
-/*
-do
-{
-IList<IWebElement> Test= SeleniumDriver.WebDriver.FindElements(By.Xpath(""));
-
-//HERE YOU CHECK IF BUTTON EXIST
-if(isElementPresented(By.Id("")))
-{
-driver.FindElement(By.Id("")).Click;
-}
-else
-{
-//if button not exists
-buttonExist = false;
-}
-}while(buttonExist);
-*/
